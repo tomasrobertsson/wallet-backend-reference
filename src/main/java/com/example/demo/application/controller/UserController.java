ec239fadd2cbc81ebf634244a82c109e6d420d20
Jesper Hellberg <extern.jesper.hellberg@digg.se>
2025-06-30 15:15:58 +0200

    feat: OpenAPI (#11)
    Unless any specific documentation is needed, the openAPI-documentation we
    get out-of-the-box is probably good enough.
    
    Add @RestController annotation and remove @Operation to use the most
    basic documentation.
    
    Signed-off-by: Jesper Hellberg <extern.jesper.hellberg@digg.se>

--- src/main/java/com/example/demo/application/controller/UserController.java
+++ src/main/java/com/example/demo/application/controller/UserController.java
@@ -11,14 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.application.model.CreateUserDto;
 import com.example.demo.application.model.UserDto;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-
 @RequestMapping("/users")
+@RestController
 public class UserController {
 
   @GetMapping("{id}")
@@ -26,13 +25,6 @@ public class UserController {
     return null;
   }
 
-  // TODO well defined openapi docs
-  // IF TOO CLUTTERY MOVE TO INTERFACE?
-  @Operation(responses = {
-      @ApiResponse(responseCode = "201"),
-      @ApiResponse(responseCode = "4xx"),
-      @ApiResponse(responseCode = "5xx"),
-  })
   @PostMapping
   public ResponseEntity<UserDto> addUser(CreateUserDto user) {
     return ResponseEntity.created(URI.create("/users/" + "id")).build(); // ?
